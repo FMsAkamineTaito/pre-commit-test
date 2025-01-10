a003341da7bd243aa9452e9c0334dd55da292b1b
AkamineTaito <>
2025-01-10 16:28:13 +0900

    a

--- PRStatusChecker/pr_status_checker.py
+++ PRStatusChecker/pr_status_checker.py
@@ -134,10 +134,14 @@ class PRStatusChecker:
             status_json = cls._run_command(["gh", "pr", "view", str(pr_number), "--json", "statusCheckRollup"])
             status_data = json.loads(status_json).get("statusCheckRollup", None)
 
+            print("## status data")
+            print(status_data)
             if not status_data:
+                print("## not status data")
                 return True
 
-            latest_conclusion = max(status_data, key=lambda x: datetime.fromisoformat(x["completedAt"].replace("Z", "+00:00"))).get("conclusion", False)
+            latest_conclusion = max(status_data, key=lambda x: datetime.fromisoformat(x["completedAt"].replace("Z", ""))).get("conclusion", False)
+            print("## latest conclusion ", latest_conclusion)
             return latest_conclusion
 
         except subprocess.CalledProcessError as e:
