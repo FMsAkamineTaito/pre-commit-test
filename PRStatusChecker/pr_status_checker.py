48d6dc034d66af06c68ff63a9d7730d1110fa5bb
AkamineTaito <>
2025-01-14 11:43:36 +0900

    a

--- PRStatusChecker/pr_status_checker.py
+++ PRStatusChecker/pr_status_checker.py
@@ -151,8 +151,9 @@ class PRStatusChecker:
     @classmethod
     def reset_to_before_merge(cls):
         """差分を破棄して前の作業ブランチに戻る"""
-        # cls._run_command(["git", "merge", "--abort"])
-        # cls._run_command(["git", "checkout", "-"])
+        print("## 元のブランチに戻ります")
+        cls._run_command(["git", "merge", "--abort"])
+        cls._run_command(["git", "checkout", "-"])
 
     @classmethod
     def is_fms_member(cls, pr_number: str):
