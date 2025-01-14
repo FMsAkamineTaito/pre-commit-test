0977754ed2b8dbd63ce66b1933d23a6592c9998f
AkamineTaito <>
2025-01-14 11:30:14 +0900

    a

--- PRStatusChecker/pr_status_checker.py
+++ PRStatusChecker/pr_status_checker.py
@@ -151,7 +151,6 @@ class PRStatusChecker:
     @classmethod
     def reset_to_before_merge(cls):
         """差分を破棄して前の作業ブランチに戻る"""
-        cls._run_command(["git", "reset", "--merge"])
         cls._run_command(["git", "merge", "--abort"])
         cls._run_command(["git", "checkout", "-"])
 
